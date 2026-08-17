f3be97b24fd64413bce2cae2726571dba24ddc92
parvezsaiyed91 <parvezsaiyed91@gmail.com>
2026-08-17 15:56:42 +0530

    For First Jenkins.

--- src/com/demo/git/SampleJava.java
+++ src/com/demo/git/SampleJava.java
@@ -3,8 +3,6 @@ package com.demo.git;
 public class SampleJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Sayed");
-		
-		System.out.println("Hello Sayed");
+		System.out.println("Hello Project - Github to Jenkins Build Pipeline");
 	}
 }
